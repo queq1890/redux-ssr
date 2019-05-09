efe0b9ee98626d1fee8de8c3b2dee21493dc121d
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-09 18:27:07 +0700

    add link on Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-  return <div>There is home.</div>;
+  return (
+    <div>
+      There is home.
+      <Link to="/contact">another route</Link>
+    </div>
+  );
 };
 
 export default Home;
