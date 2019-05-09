cbfd938eb5a87c352206408954db09676dbc3f33
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-09 18:22:52 +0700

    client side endpoint

---
+++ src/client.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import Routes from './Routes';
+import { initState } from './redux/rootReducer';
+import { configureStore } from './isomorphic/store';
+
+declare var window: { INITIAL_STATE: any };
+
+const initialState: any = window.INITIAL_STATE || initState();
+delete window.INITIAL_STATE;
+
+const preload = configureStore(initialState, createBrowserHistory());
+
+ReactDOM.hydrate(
+  <Provider store={preload.store}>
+    <ConnectedRouter history={preload.history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
