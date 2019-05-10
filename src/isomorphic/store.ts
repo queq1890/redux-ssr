aa3ecffbced7ab9a27f2b00274569fc1a49c17d4
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-10 13:57:26 +0700

    Dev tool settings

--- src/isomorphic/store.ts
+++ src/isomorphic/store.ts
@@ -1,11 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { History } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import { createRootReducer, RootState } from '../redux/rootReducer';
 
+const composeEnhancers =
+  typeof (window as any) === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
 export const configureStore = (initialState: RootState, history: History) => {
   const middleWare = applyMiddleware(routerMiddleware(history));
-  const store = createStore(createRootReducer(history), initialState, middleWare);
+  const store = createStore(createRootReducer(history), initialState, composeEnhancers(middleWare));
 
   return {
     history,
