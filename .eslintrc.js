e79a5ff7432b9056be80776da7c761e9758e79f4
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-14 15:20:31 +0700

    chore: add env to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
   plugins: ['@typescript-eslint', 'react'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     sourceType: 'module',
-    project: './tsconfig.client.json'
+    project: './tsconfig.client.json',
+  },
+  env: {
+    browser: true,
+    node: true,
   },
   rules: {
     'react/react-in-jsx-scope': 0,
@@ -15,6 +24,6 @@ module.exports = {
     '@typescript-eslint/indent': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/adjacent-overload-signatures': 'error',
-    'prettier/prettier': ['error', { singleQuote: true }]
-  }
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
 };
