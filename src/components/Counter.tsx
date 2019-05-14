76b18a90eb724d4f5c11b4caef0448649998c23c
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-14 14:32:46 +0700

    use Increment() & decrement() in counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,13 +2,31 @@ import * as React from 'react';
 
 export interface CounterProps {
   count: number;
+  decrement: () => void;
+  increment: () => void;
 }
 
-const Counter: React.FC<CounterProps> = props => (
-  <div>
-    heyguys
-    {props.count}
-  </div>
-);
+const isDecrementable = (count: number): boolean => {
+  return count > 0;
+};
+
+const Counter: React.FC<CounterProps> = props => {
+  return (
+    <div>
+      heyguys
+      {props.count}
+      <button onClick={() => props.increment()}>increment</button>
+      <button
+        onClick={() => {
+          if (isDecrementable(props.count)) {
+            props.decrement();
+          }
+        }}
+      >
+        decrement
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
