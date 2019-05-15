eb74d4b897df34bcba7c6fcc668d8e1e508f31ef
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-15 16:51:22 +0700

    fix: change condition og props.animals

--- src/components/AnimalsList.tsx
+++ src/components/AnimalsList.tsx
@@ -9,7 +9,13 @@ export interface AnimalsListProps {
 const AnimalsList = (props: AnimalsListProps) => (
   <div>
     <button onClick={() => props.getAnimalsStart()}>fetch animal</button>
-    {props.animals && props.animals.map(animal => <p key={animal.id}>{animal}</p>)}
+    {props.animals.length > 0 &&
+      props.animals.map(animal => (
+        <div key={animal.id}>
+          <p>{animal.kind}</p>
+          <p>{animal.name}</p>
+        </div>
+      ))}
   </div>
 );
 
