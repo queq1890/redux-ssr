389cbef91443ed72fced1abc1cd972321687be2e
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-15 16:50:52 +0700

    run saga in client & server

--- src/isomorphic/store.ts
+++ src/isomorphic/store.ts
@@ -1,7 +1,15 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware, Store } from 'redux';
 import { History } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import { createRootReducer, RootState } from '../redux/rootReducer';
+import createSagaMiddleware, { END } from 'redux-saga';
+
+export const sagaMiddleware = createSagaMiddleware();
+
+interface SSRStore extends Store {
+  runSaga: any;
+  close: () => void;
+}
 
 const composeEnhancers =
   typeof (window as any) === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -9,8 +17,11 @@ const composeEnhancers =
     : compose;
 
 export const configureStore = (initialState: RootState, history: History) => {
-  const middleWare = applyMiddleware(routerMiddleware(history));
-  const store = createStore(createRootReducer(history), initialState, composeEnhancers(middleWare));
+  const middleWare = applyMiddleware(routerMiddleware(history), sagaMiddleware);
+  const store: SSRStore = createStore(createRootReducer(history), initialState, composeEnhancers(middleWare));
+
+  store.runSaga = sagaMiddleware.run;
+  store.close = () => store.dispatch(END);
 
   return {
     history,
