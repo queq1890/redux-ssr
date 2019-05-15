d81821d6c11daffc979fedbfd75122c32133f702
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-15 17:24:20 +0700

    fix: Change directory name

--- src/redux/animals/sagas.ts
+++ src/redux/animals/sagas.ts
@@ -1,7 +1,7 @@
 import { put, call, takeLatest } from 'redux-saga/effects';
 import * as Action from '../animals/constants';
 import { getAnimals } from '../animals/actions';
-import AnimalApiService from '../../Services/AnimalAPIService';
+import AnimalApiService from '../../services/AnimalAPIService';
 
 const APIClient = new AnimalApiService();
 
